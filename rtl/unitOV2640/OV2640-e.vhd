6bd03d0a7c35e044f1fd3825df49508631c8ded2
Alexander Entinger <cto@lxrobotics.com>
2020-01-23 08:44:59 +0100

    Renaming OV2640 modules for better clarity

--- rtl/unitOV2640/OV2640-e.vhd
+++ rtl/unitOV2640/OV2640-e.vhd
@@ -14,11 +14,10 @@ entity OV2640 is
   port (
     iClk      : in std_ulogic;
     inReset   : in std_ulogic;
-    iPixelClk : in std_ulogic;
-    iHref     : in std_ulogic;
-    iVsync    : in std_ulogic;
-    iData     : in std_ulogic_vector(7 downto 0);
-    oCamClk   : out std_ulogic
+    iCamPCLK  : in std_ulogic;                    -- Pixel Clock
+    iCamHREF  : in std_ulogic;                    -- Horizontal Reference
+    iCamVSYNC : in std_ulogic;                    -- Vertical Synchronisation
+    iCamDATA  : in std_ulogic_vector(7 downto 0); -- Pixel Data
+    oCamXCLK  : out std_ulogic                    -- System Clock
   );
 end OV2640;
-
