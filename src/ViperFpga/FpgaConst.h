86b346c56b98ee5a1d0901de9c020bbf449952aa
Alexander Entinger <cto@lxrobotics.com>
2020-04-01 08:39:52 +0200

    Extracting constant definitions into FpgaConst

--- src/ViperFpga/FpgaConst.h
+++ src/ViperFpga/FpgaConst.h
@@ -4,20 +4,11 @@
  * @license LGPL 3.0
  */
 
-#ifndef ARDUINO_VIPER_FPGA_IO_H_
-#define ARDUINO_VIPER_FPGA_IO_H_
-
 /**************************************************************************************
  * INCLUDE
  **************************************************************************************/
 
 #include <stdint.h>
-
-#undef max
-#undef min
-#include <functional>
-
-#include "FpgaConst.h"
 
 /**************************************************************************************
  * NAMESPACE
@@ -30,32 +21,12 @@ namespace ViperFpga
  * TYPEDEF
  **************************************************************************************/
 
-typedef std::function<void()>                 SpiSelectFunc;
-typedef std::function<void()>                 SpiDeselectFunc;
-typedef std::function<uint8_t(uint8_t const)> SpiTransferFunc;
-
-/**************************************************************************************
- * CLASS DECLARATION
- **************************************************************************************/
-
-class FpgaIo
+enum class Register : uint8_t
 {
-
-public:
-
-  FpgaIo(SpiSelectFunc select, SpiDeselectFunc deselect, SpiTransferFunc transfer);
-
-
-  uint8_t read (Register const reg);
-  void    write(Register const reg, uint8_t const reg_val);
-
-
-private:
-
-  SpiSelectFunc _select;
-  SpiDeselectFunc _deselect;
-  SpiTransferFunc _transfer;
-
+  FPGA_REV_NUM            = 0x00,
+  RGB_LED_RED_INTENSITY   = 0x01,
+  RGB_LED_GREEN_INTENSITY = 0x02,
+  RGB_LED_BLUE_INTENSITY  = 0x03
 };
 
 /**************************************************************************************
@@ -63,5 +34,3 @@ private:
  **************************************************************************************/
 
 } /* namespace ViperFpga */
-
-#endif /* ARDUINO_VIPER_FPGA_IO_H_ */
