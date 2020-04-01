ac6d35b974e348e8f1e52ca84c8c9e46056cf237
Alexander Entinger <cto@lxrobotics.com>
2020-04-01 08:40:46 +0200

    Removing code authors notice to foster a sense of collaborative ownership

--- src/ViperFpga/FpgaIo.h
+++ src/ViperFpga/FpgaIo.h
@@ -1,6 +1,5 @@
 /**
  * @brief   Arduino library containing FPGA configuration and MCU interface code for the MKR VIDOR 4000 based Viper quadcopter.
- * @author  Alexander Entinger, MSc / LXRobotics GmbH
  * @license LGPL 3.0
  */
 
