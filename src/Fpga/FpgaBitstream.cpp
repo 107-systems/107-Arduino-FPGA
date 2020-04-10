903234c1b402d8a1b2b9b0a88bc77fc3d1707ab3
Alexander Entinger <cto@lxrobotics.com>
2020-04-10 10:11:23 +0200

    Renaming signature.h to bitstream_signature.h and adding comments for better understanding how this works

--- src/Fpga/FpgaBitstream.cpp
+++ src/Fpga/FpgaBitstream.cpp
@@ -13,6 +13,26 @@
  * DEFINES
  **************************************************************************************/
 
+/* A signature has the following structure:
+ *
+ *   typedef struct __attribute__((packed)) {
+ *     uint32_t offset;
+ *     uint32_t length;
+ *     uint32_t sha256sum[8];
+ *     uint32_t type;
+ *     uint32_t force;
+ *   } ExternalFlashSignature;
+ *
+ * The byte array 'signatures' contains 3 'ExternalFlashSignature'
+ *
+ *   const unsigned char signatures[4096] =
+ *   {
+ *     SIGNATURE_BOOTLOADER,
+ *     SIGNATURE_APP,        // The FPGA bitstream
+ *     SIGNATURE_USER_DATA
+ *   };
+ */
+
 #define SIGNATURE_NO_DATA \
   0xFF, 0xFF, 0xFF, 0xFF, \
   0xFF, 0xFF, 0xFF, 0xFF, \
@@ -28,14 +48,6 @@
  * CONSTANTS
  **************************************************************************************/
 
-/* The signature has the following structure:
- *   - uint32_t offset;
- *   - uint32_t length;
- *   - uint32_t sha256sum[8];
- *   - uint32_t type;
- *   - uint32_t force;
- */
-
 /* The bootloader code which uses the signature to determine whether or not to load
  * to the FPGA can be found here:
  * https://github.com/arduino/ArduinoCore-samd/blob/020b419fc1dbca49b23b7c0b5e47d50fb2a04485/bootloaders/zero/sam_ba_monitor.c#L443:L512
@@ -43,7 +55,9 @@
 
 __attribute__ ((used, section(".fpga_bitstream_signature")))
 const unsigned char signatures[4096] = {
-  #include "signature.h"
+  NO_BOOTLOADER,
+  #include "bitstream_signature.h"
+  NO_USER_DATA,
 };
 
 __attribute__ ((used, section(".fpga_bitstream")))
