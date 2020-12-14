9325158070b13758f876164f9fb4df6f4d212546
Alexander Entinger <cto@lxrobotics.com>
2020-12-14 14:27:47 +0100

    Changing SPI Slave to have 3 states - Instruction, Address and Data - which allows the usage of 256 reg addresses + additional SPI relayed commands

--- extras/fpga/rtl/grpSpi/unitSpiSlave/SpiSlave-Rtl-a.vhd
+++ extras/fpga/rtl/grpSpi/unitSpiSlave/SpiSlave-Rtl-a.vhd
@@ -14,42 +14,47 @@ use work.viper.all;
 
 architecture Rtl of SpiSlave is
 
-    type aSpiSlaveState is (Idle, Address, Data);
+    type aSpiSlaveState is (Idle, Command, Address, Data);
     subtype aSpiBitCnt is unsigned (2 downto 0);
+    subtype aSpiCommand is std_ulogic_vector (7 downto 0);
 
     type aRegSet is record
         SpiSlaveState : aSpiSlaveState;
-        SpiSck        : std_ulogic;
+        iSpiSck       : std_ulogic;
+        oSpiMosi      : std_ulogic;
         SpiBitCnt     : aSpiBitCnt;
+        SpiRegCommand : std_ulogic_vector(7 downto 0);
         SpiRegAddr    : std_ulogic_vector(7 downto 0);
         SpiRegData    : std_ulogic_vector(7 downto 0);
     end record;
     -----------------------------------------------------------
     constant cInitRegSet : aRegSet := (
         SpiSlaveState => Idle,
-        SpiSck        => '0',
+        iSpiSck       => '0',
+        oSpiMosi      => '0',
         SpiBitCnt     => to_unsigned(0, aSpiBitCnt'length),
+        SpiRegCommand => (others => '0'),
         SpiRegAddr    => (others => '0'),
         SpiRegData    => (others => '0')
     );
+    constant cCommandWrite : aSpiCommand := "00000010"; -- WRITE = 0x02
+    constant cCommandRead  : aSpiCommand := "00000011"; -- READ  = 0x03
     -----------------------------------------------------------
     signal R, NextR             : aRegSet;
     signal sck_rising_edge      : std_ulogic;
-    signal spi_slave_write      : std_ulogic;
-    signal spi_slave_read       : std_ulogic;
     signal spi_bit_cnt_overflow : std_ulogic;
     -----------------------------------------------------------
 begin
     -----------------------------------------------------------
-    sck_rising_edge <= iSCK and not R.SpiSck;
-    spi_slave_write <= R.SpiRegAddr(R.SpiRegAddr'high);
-    spi_slave_read <= not spi_slave_write;
+    sck_rising_edge <= iSCK and not R.iSpiSck;
     spi_bit_cnt_overflow <= cActivated when R.SpiBitCnt = to_unsigned(7, aSpiBitCnt'length) else cInactivated;
     -----------------------------------------------------------
-    comb : process(R, iMOSI, iSCK, inCS, sck_rising_edge, spi_slave_write, spi_slave_read, spi_bit_cnt_overflow) is
+    comb : process(R, iMOSI, iSCK, inCS, sck_rising_edge, spi_bit_cnt_overflow) is
     begin
         -------------------------------------------------------
         NextR <= R;
+        NextR.oSpiMosi <= '0';
+        -------------------------------------------------------
         if sck_rising_edge = cActivated then
             NextR.SpiBitCnt <= R.SpiBitCnt + 1;
         end if;
@@ -58,29 +63,38 @@ begin
             ---------------------------------------------------
             when Idle =>
                 if inCS = cnActivated then
-                    NextR.SpiSlaveState <= Address;
-                    NextR.SpiBitCnt <= to_unsigned(0, aSpiBitCnt'length);
-                    NextR.SpiRegAddr <= (others => '0');
-                    NextR.SpiRegData <= (others => '0');
+                    NextR.SpiSlaveState <= Command;
+                    NextR.SpiBitCnt     <= to_unsigned(0, aSpiBitCnt'length);
+                    NextR.SpiRegCommand <= (others => '0');
+                    NextR.SpiRegAddr    <= (others => '0');
+                    NextR.SpiRegData    <= (others => '0');
+                end if;
+            ---------------------------------------------------
+            when Command =>
+                if sck_rising_edge = cActivated then
+                    NextR.SpiRegCommand <= R.SpiRegCommand(R.SpiRegCommand'high-1 downto 0) & iMOSI;
+                end if;
+                if spi_bit_cnt_overflow = cActivated then
+                    NextR.SpiSlaveState <= Data;
                 end if;
             ---------------------------------------------------
             when Address =>
                 if sck_rising_edge = cActivated then
                     NextR.SpiRegAddr <= R.SpiRegAddr(R.SpiRegAddr'high-1 downto 0) & iMOSI;
-                    if spi_bit_cnt_overflow = cActivated then
-                        NextR.SpiSlaveState <= Data;
-                    end if;
+                end if;
+                if spi_bit_cnt_overflow = cActivated then
+                    NextR.SpiSlaveState <= Data;
                 end if;
             ---------------------------------------------------
             when Data =>
                 if sck_rising_edge = cActivated then
                     -------------------------------------------
-                    if spi_slave_write = cActivated then
+                    if R.SpiRegCommand = cCommandWrite then
                         NextR.SpiRegData <= R.SpiRegData(R.SpiRegData'high-1 downto 0) & iMOSI;
                     end if;
                     -------------------------------------------
-                    if spi_slave_read = cActivated then
-                        oMISO <= R.SpiRegData(to_integer(R.SpiBitCnt));
+                    if R.SpiRegCommand = cCommandRead then
+                        NextR.oSpiMosi <= R.SpiRegData(to_integer(R.SpiBitCnt));
                     end if;
                     -------------------------------------------
                 end if;
@@ -106,6 +120,8 @@ begin
     end process sequ;
 
     -----------------------------------------------------------
+
+    oMISO <= R.oSpiMosi;
 
 end Rtl;
   
